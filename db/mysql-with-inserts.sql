e836f6a39adc177a5bf2842a0c44f16eb957aa60
Santi <santii810@hotmail.com>
2019-02-28 17:57:41 +0100

    Modified database to save pets data

--- db/mysql-with-inserts.sql
+++ db/mysql-with-inserts.sql
@@ -14,6 +14,18 @@ CREATE TABLE `daaexample`.`users` (
 	PRIMARY KEY (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `daaexample`.`pet` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner` VARCHAR(50) NOT NULL,
+  `name` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_pet_person` (`owner`),
+  CONSTRAINT `FK_pet_person` FOREIGN KEY (`owner`) REFERENCES `people` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
 GRANT ALL ON `daaexample`.* TO 'daa'@'localhost' IDENTIFIED BY 'daa';
 
 INSERT INTO `daaexample`.`people` (`id`,`name`,`surname`) VALUES (0,'Antón','Pérez');
--- db/mysql.sql
+++ db/mysql.sql
@@ -14,4 +14,14 @@ CREATE TABLE `daaexample`.`users` (
 	PRIMARY KEY (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `daaexample`.`pet` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner` int(11) NOT NULL,
+  `name` VARCHAR (50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_pet_person` (`owner`),
+  CONSTRAINT `FK_pet_person` FOREIGN KEY (`owner`) REFERENCES `people` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 GRANT ALL ON `daaexample`.* TO 'daa'@'localhost' IDENTIFIED BY 'daa';
